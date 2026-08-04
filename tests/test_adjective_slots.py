c25a70e589c5d87f8003eee7ea676bd90e301ac8
Mats Järgenstedt <matzikratzi@gmail.com>
2026-08-05 00:31:02 +0200

    Test generic adjective notation slots

--- tests/test_adjective_slots.py
+++ tests/test_adjective_slots.py
@@ -12,26 +12,14 @@ class AdjectiveSlotsTests(unittest.TestCase):
         return slots
 
     def test_regular_t_a_applies_neuter_spelling(self) -> None:
-        slots = self.parse("glad", "+t +a")
-        self.assertEqual(("glad", "glatt", "glada"), slots.written_forms())
-        self.assertEqual("regular_t_a", slots.rule)
-
-    def test_final_rd_and_ld_drop_d_before_t(self) -> None:
+        self.assertEqual(("glad", "glatt", "glada"), self.parse("glad", "+t +a").written_forms())
         self.assertEqual(("hård", "hårt", "hårda"), self.parse("hård", "+t +a").written_forms())
         self.assertEqual(("vild", "vilt", "vilda"), self.parse("vild", "+t +a").written_forms())
-
-    def test_other_final_d_becomes_tt(self) -> None:
         self.assertEqual(("röd", "rött", "röda"), self.parse("röd", "+t +a").written_forms())
 
-    def test_unchanged_neuter(self) -> None:
-        slots = self.parse("kortväxt", "n. +, +a")
-        self.assertEqual(("kortväxt", "kortväxta"), slots.written_forms())
-        self.assertEqual(3, len(slots.forms))
-
-    def test_regular_tt_a(self) -> None:
+    def test_basic_patterns(self) -> None:
+        self.assertEqual(("kortväxt", "kortväxta"), self.parse("kortväxt", "n. +, +a").written_forms())
         self.assertEqual(("blå", "blått", "blåa"), self.parse("blå", "+tt +a").written_forms())
-
-    def test_regular_t_ma(self) -> None:
         self.assertEqual(("öm", "ömt", "ömma"), self.parse("öm", "+t +ma").written_forms())
 
     def test_explicit_pairs(self) -> None:
@@ -40,24 +28,13 @@ class AdjectiveSlotsTests(unittest.TestCase):
         self.assertEqual(("osäker", "osäkert", "osäkra"), self.parse("osäker", "+t -säkra").written_forms())
         self.assertEqual(("bebodd", "bebott", "bebodda"), self.parse("bebodd", "bebott bebodda").written_forms())
 
-    def test_labelled_plural_alternatives(self) -> None:
-        slots = self.parse("akvamarinblå", "-blått, best. och: pl. + el. +a")
-        self.assertEqual(("akvamarinblå", "akvamarinblått", "akvamarinblåa"), slots.written_forms())
-
-    def test_labelled_limited_plural_suffix(self) -> None:
-        slots = self.parse("fullmäktig", "pl. +e")
-        self.assertEqual(("fullmäktig", "fullmäktige"), slots.written_forms())
-        self.assertEqual("labelled_limited_paradigm", slots.rule)
-
-    def test_labelled_limited_explicit_plural(self) -> None:
+    def test_labels_and_limited_slots(self) -> None:
+        self.assertEqual(("akvamarinblå", "akvamarinblått", "akvamarinblåa"), self.parse("akvamarinblå", "-blått, best. och: pl. + el. +a").written_forms())
+        self.assertEqual(("blå", "blått", "blåa"), self.parse("blå", "blått, best. och: pl. blå el. blåa").written_forms())
+        self.assertEqual(("fullmäktig", "fullmäktige"), self.parse("fullmäktig", "pl. +e").written_forms())
         self.assertEqual(("bitteliten", "bittesmå"), self.parse("bitteliten", "pl. bittesmå").written_forms())
-
-    def test_labelled_masculine_and_bare_definite(self) -> None:
         self.assertEqual(("främsta", "främste"), self.parse("främsta", "mask. främste").written_forms())
         self.assertEqual(("flesta",), self.parse("flesta", "best.").written_forms())
-
-    def test_full_labelled_plural_alternatives(self) -> None:
-        self.assertEqual(("blå", "blått", "blåa"), self.parse("blå", "blått, best. och: pl. blå el. blåa").written_forms())
 
     def test_single_slot_patterns(self) -> None:
         self.assertEqual(("dan", "dant"), self.parse("dan", "+t").written_forms())
@@ -67,31 +44,31 @@ class AdjectiveSlotsTests(unittest.TestCase):
         self.assertEqual(("hot", "hotta"), self.parse("hot", "neutr. +; pl. hotta").written_forms())
         self.assertEqual(("förstnämnde", "förstnämnda"), self.parse("förstnämnde", "förstnämnda").written_forms())
 
-    def test_parallel_participial_alternatives(self) -> None:
-        slots = self.parse("fasetterad", "fasetterat +e _ facetterat +e")
-        self.assertEqual(("fasetterad", "fasetterat", "fasetterade", "facetterad", "facetterat", "facetterade"), slots.written_forms())
-        self.assertEqual("parallel_alternative_paradigms", slots.rule)
-
-    def test_parallel_replacement_alternatives_in_compound(self) -> None:
-        slots = self.parse("hårdflörtad", "-flörtat +e _ -flirtat +e")
-        self.assertEqual(("hårdflörtad", "hårdflörtat", "hårdflörtade", "hårdflirtad", "hårdflirtat", "hårdflirtade"), slots.written_forms())
-
-    def test_parallel_complete_pairs(self) -> None:
+    def test_parallel_alternatives(self) -> None:
+        self.assertEqual(("fasetterad", "fasetterat", "fasetterade", "facetterad", "facetterat", "facetterade"), self.parse("fasetterad", "fasetterat +e _ facetterat +e").written_forms())
+        self.assertEqual(("hårdflörtad", "hårdflörtat", "hårdflörtade", "hårdflirtad", "hårdflirtat", "hårdflirtade"), self.parse("hårdflörtad", "-flörtat +e _ -flirtat +e").written_forms())
         self.assertEqual(("ledsen", "ledset", "ledsna", "lesset", "lessna"), self.parse("ledsen", "ledset ledsna _ lesset lessna").written_forms())
 
-    def test_rejects_ambiguous_parallel_suffix_branch(self) -> None:
-        self.assertIsNone(interpret_simple_adjective_slots({"normaliserat_ord": "sjangdobel", "text": "+t sjangdobla _ +t schangdobla"}))
+    def test_generic_comment_and_replacement_notation(self) -> None:
+        self.assertEqual(("perenn", "perent", "perenna"), self.parse("perenn", "perent [-en>t] +a").written_forms())
+        self.assertEqual(("hög", "högt", "höga", "högre", "högst"), self.parse("hög", "högt [hök>t] höga, högre högst [hök>st]").written_forms())
+        self.assertEqual(("reliabel", "reliabelt", "reliabla"), self.parse("reliabel", "+-t reliabla").written_forms())
 
-    def test_comparison_only_suffixes(self) -> None:
+    def test_generic_explicit_and_alternative_slots(self) -> None:
+        self.assertEqual(("juste", "justa"), self.parse("juste", "n. +, justa").written_forms())
+        self.assertEqual(("bemälde", "bemälda", "bemälta"), self.parse("bemälde", "bemälda el. bemälta").written_forms())
+        self.assertEqual(("trång", "trångt", "trängre", "trångare", "trängst", "trångast"), self.parse("trång", "+t, trängre H +are, trängst H +ast").written_forms())
+
+    def test_parallel_variant_with_shared_suffix_notation(self) -> None:
+        self.assertEqual(
+            ("sjangdobel", "sjangdobelt", "sjangdobla", "schangdobel", "schangdobelt", "schangdobla"),
+            self.parse("sjangdobel", "+t sjangdobla _ +t schangdobla").written_forms(),
+        )
+
+    def test_comparison(self) -> None:
         self.assertEqual(("ringa", "ringare", "ringast"), self.parse("ringa", "komp. +re, superl. +st").written_forms())
-
-    def test_comparison_only_explicit_forms(self) -> None:
         self.assertEqual(("få", "färre", "färst"), self.parse("få", "komp. färre, superl. färst").written_forms())
-
-    def test_positive_with_labelled_comparison(self) -> None:
         self.assertEqual(("förnäm", "förnämt", "förnäma", "förnämare", "förnämst", "förnämast"), self.parse("förnäm", "+t +a, komp. +are, superl. +st H +ast").written_forms())
-
-    def test_explicit_positive_and_comparison(self) -> None:
         self.assertEqual(("god", "gott", "goda", "bättre", "bäst"), self.parse("god", "gott goda, bättre bäst").written_forms())
 
     def test_rejects_truncated_comparison(self) -> None:
