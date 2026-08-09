3b9bb3fe8b7a1f88b5cfa5b678fe6f2ddb84a580
Mats Järgenstedt <matzikratzi@gmail.com>
2026-08-09 22:54:30 +0200

    Use evidenced noun variant generation

--- src/swedish_wordlist_tools/refresh_noun_validation.py
+++ src/swedish_wordlist_tools/refresh_noun_validation.py
@@ -5,13 +5,14 @@ import sys
 
 
 MODULES = (
-    "swedish_wordlist_tools.generate_noun_forms",
+    "swedish_wordlist_tools.generate_noun_forms_variants",
     "swedish_wordlist_tools.revalidate_direct_forms",
     "swedish_wordlist_tools.rebaseline_noun_validation",
     "swedish_wordlist_tools.analyze_remaining_noun_notations",
     "swedish_wordlist_tools.analyze_null_noun_ordkl",
     "swedish_wordlist_tools.analyze_ordkl_field",
     "swedish_wordlist_tools.analyze_surface_lemma_variants",
+    "swedish_wordlist_tools.analyze_surface_variant_roles",
     "swedish_wordlist_tools.analyze_remaining_noun_provenance",
 )
 
