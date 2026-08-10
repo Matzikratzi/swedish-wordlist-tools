3d35cd56efc41b808735dbff8911cebae659c5c1
Mats Järgenstedt <matzikratzi@gmail.com>
2026-08-10 08:39:02 +0200

    Include unsupported noun analysis in refresh

--- src/swedish_wordlist_tools/refresh_noun_validation.py
+++ src/swedish_wordlist_tools/refresh_noun_validation.py
@@ -9,6 +9,7 @@ MODULES = (
     "swedish_wordlist_tools.revalidate_direct_forms",
     "swedish_wordlist_tools.rebaseline_noun_validation",
     "swedish_wordlist_tools.analyze_remaining_noun_notations",
+    "swedish_wordlist_tools.analyze_unsupported_nouns",
     "swedish_wordlist_tools.analyze_null_noun_ordkl",
     "swedish_wordlist_tools.analyze_ordkl_field",
     "swedish_wordlist_tools.analyze_surface_lemma_variants",
