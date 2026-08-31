966a8a9fad63af4aed0ce355dbfc6c5d123303e1
Mats Järgenstedt <matzikratzi@gmail.com>
2026-08-31 13:35:04 +0200

    Fix non-positive page selection test

--- tests/test_ocr_probe_white_gap_pages.py
+++ tests/test_ocr_probe_white_gap_pages.py
@@ -10,7 +10,7 @@ class WhiteGapPagesProbeTests(unittest.TestCase):
         self.assertEqual(parse_pages("1,3,5-7,6,10-9"), [1, 3, 5, 6, 7, 9, 10])
 
     def test_parse_pages_ignores_non_positive_pages(self) -> None:
-        self.assertEqual(parse_pages("0,-1,2"), [1, 2])
+        self.assertEqual(parse_pages("0,-1,2"), [2])
 
 
 if __name__ == "__main__":
